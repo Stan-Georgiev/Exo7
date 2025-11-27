278ad5fca633bf741064ea05d271d49ecedd00c6
Tristan086 <6292338@cstjean.qc.ca>
2025-11-27 18:37:00 -0500

    I am almost failing web

--- src/ControleTemperature.java
+++ src/ControleTemperature.java
@@ -1,2 +1,16 @@
-public class ControleTemperature {
+public class ControleTemperature implements IServiceThermostat, IServiceTemperature{
+    @Override
+    public double getTemperature() {
+        return 0;
+    }
+
+    @Override
+    public void demarrerChauffage() {
+
+    }
+
+    @Override
+    public void arreterChauffage() {
+
+    }
 }
